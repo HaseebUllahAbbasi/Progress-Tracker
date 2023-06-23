d05e1bbf3bce22f35c447df1ca41c45869203300
haseebullahabbasi <haseebabbasi00@gmail.com>
2023-06-23 18:06:48 +0500

    submodules added

--- setup.sh
+++ setup.sh
@@ -23,6 +23,10 @@ if ! is_submodule_cloned "app-progress-tracker"; then
     git clone git@github.com:HaseebUllahAbbasi/app-progress-tracker.git
 fi
 
+if ! is_submodule_cloned "desktop-progress-tracker"; then
+    git clone git@github.com:HaseebUllahAbbasi/desktop-progress-tracker.git
+fi
+
 # Update and checkout to the development branches
 git submodule update --init --recursive
 git submodule foreach --recursive git checkout development
